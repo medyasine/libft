f98093259ed212fd24c453236d8c7d7a7b4f6097
medyasine <yassinmohamad89@gmail.com>
2024-10-25 15:13:25 +0100

    strlcpy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,29 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: masnus <masnus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 18:28:02 by masnus            #+#    #+#             */
-/*   Updated: 2024/10/22 19:25:19 by masnus           ###   ########.fr       */
+/*   Created: 2024/10/22 19:07:37 by masnus            #+#    #+#             */
+/*   Updated: 2024/10/22 19:25:12 by masnus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlcpy(char *dst, char *src, int dstsize)
+int	strlcat(char *dst, const char *src, int dstsize)
 {
-	int	i;
 	int	src_len;
+	int	dst_len;
+	int	i;
 
-	i = 0;
+	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	while (src[i] && i <= destsize - 1)
-	{
-		dst[i] = src[i++];
-	}
-	if (dstsize != 0)
-		dst[i] = '\0';
-	return (src_len);
+	i = 0;
+	if (dst_len == dstsize)
+		return (dst_len + src_len);
+	while (src[i] && i < dstsize - dst_len - 1)
+		dst[dst_len + i] = src[i++];
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
