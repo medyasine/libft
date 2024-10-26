b2b3090abf746bb3fd63b1192cece08f90697284
medyasine <yassinmohamad89@gmail.com>
2024-10-26 13:01:26 +0100

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,24 @@
 /*   By: masnus <masnus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 12:34:27 by masnus            #+#    #+#             */
-/*   Updated: 2024/10/26 12:34:28 by masnus           ###   ########.fr       */
+/*   Updated: 2024/10/26 12:57:23 by masnus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t len;
+	size_t i;
+
+	if(!s || !f)
+		return;
+	len = ft_strlen(s);
+	i = 0;
+	while(i < len)
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
